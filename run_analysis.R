e1f2ada58d88ea2b1a158d6fdc4fe4c96fe6ae08
diegocgaona <diego@diegogaona.com>
2015-02-19 00:19:20 -0200

    Updated comments
    This commit have the script version with some changes in comments.

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,5 @@
 ## run_analysis.R
-## The script do the following with data from the sensor of a Samsung Galaxy S2:
-## 1. Merges the training and the test sets to create one data set.
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-## 3. Uses descriptive activity names to name the activities in the data set
-## 4. Appropriately labels the data set with descriptive variable names. 
-## 5. From the data set in step 4, creates a second, independent tidy data set with the 
-##    average of each variable for each activity and each subject.
-
-
+## For more info, see the README.md in https://github.com/diegocgaona/Get_Data_Course_Assignment/
 ## Check if the necessary librarys are loaded, if not, require load it
 require(dplyr)
 require(plyr)
